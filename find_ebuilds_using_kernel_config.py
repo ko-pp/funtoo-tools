73ab4e2f84691e13a742d71db7c7fe1189663417
Kévin Beaubestre <kevin@beaubestre.fr>
2018-02-01 14:08:40 +0100

    Add: handy script to know what to rebuild when changing kernel

---
+++ find_ebuilds_using_kernel_config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import portage
+
+
+dbapi = portage.db[portage.root]["vartree"].dbapi
+packages = dbapi.cp_all()
+tree = [dbapi.match(package) for package in packages]
+for package in tree:
+    for version in package:
+        inherited = dbapi.aux_get(version, ["INHERITED"])
+        for inherit in inherited:
+            if 'linux-info' in inherit:
+                print('={version}'.format(version=version))
+
